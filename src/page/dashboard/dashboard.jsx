f4aef63740168a9ab271944c22bbf713ef1c7bf5
alizarazot <alizarazot@ufpso.edu.co>
2025-09-18 14:18:52 -0500

    Add basic menu

--- src/page/dashboard/dashboard.jsx
+++ src/page/dashboard/dashboard.jsx
@@ -1,26 +1,35 @@
 import { Link } from "react-router";
 
+import { DashboardMenu } from "./menu";
+
 export function Dashboard() {
   return (
     <>
-      <nav className="navbar navbar-expand-sm">
-        <div className="container-fluid">
-          <Link className="navbar-brand text-primary" to="/">
-            Dashboard
-          </Link>
+      <div className="min-vh-100">
+        <nav className="navbar navbar-expand-sm">
+          <div className="container-fluid">
+            <Link className="navbar-brand text-primary" to="/">
+              Dashboard
+            </Link>
 
-          {/* TODO: Login. */}
-          <form>
-            <button className="btn btn-outline-dark" disabled>
-              Logout
-            </button>
-          </form>
+            {/* TODO: Login. */}
+            <form>
+              <button className="btn btn-outline-dark" disabled>
+                Logout
+              </button>
+            </form>
+          </div>
+        </nav>
+
+        <div className="d-flex align-self-stretch">
+          <div className="px-2">
+            <DashboardMenu className></DashboardMenu>
+          </div>
+          <div className="container my-3">TODO: Add main section.</div>
         </div>
-      </nav>
+      </div>
 
-      <div className="container my-3">TODO: Add main section.</div>
-
-      <footer class="container-fluid py-5 bg-info-subtle text-center fixed-bottom">
+      <footer className="container-fluid py-5 bg-info-subtle text-center">
         <p>
           This is free and unencumbered software released into the public
           domain.
