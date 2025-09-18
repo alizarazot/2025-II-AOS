533256906a37bd34b8cbd9c5a4630886f665e207
alizarazot <alizarazot@ufpso.edu.co>
2025-09-18 12:04:16 -0500

    Migrate from CRA to Vite

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "AOS";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+  in {
+    devShells.${system}.default = let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      pkgs.mkShell {
+        packages = [
+          pkgs.nodePackages.nodejs
+        ];
+      };
+  };
+}
