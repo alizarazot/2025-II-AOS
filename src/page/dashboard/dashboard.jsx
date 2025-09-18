ceb624e9e5258f5db63812d02dd3e0310fa362b7
alizarazot <alizarazot@ufpso.edu.co>
2025-09-18 13:18:47 -0500

    Add basic dashboard structure

--- src/page/dashboard/dashboard.jsx
+++ src/page/dashboard/dashboard.jsx
@@ -1,13 +1,31 @@
+import { Link } from "react-router";
+
 export function Dashboard() {
   return (
     <>
-      <div className="grid mt-3 text-center">
-        <h1> Dashboard </h1>
+      <nav className="navbar navbar-expand-sm">
+        <div className="container-fluid">
+          <Link className="navbar-brand text-primary" to="/">
+            Dashboard
+          </Link>
+
+          {/* TODO: Login. */}
+          <form>
+            <button className="btn btn-outline-dark" disabled>
+              Logout
+            </button>
+          </form>
+        </div>
+      </nav>
+
+      <div className="container my-3">TODO: Add main section.</div>
+
+      <footer class="container-fluid py-5 bg-info-subtle text-center fixed-bottom">
         <p>
-          {" "}
-          Go to <a href="/login"> login</a>.{" "}
+          This is free and unencumbered software released into the public
+          domain.
         </p>
-      </div>
+      </footer>
     </>
   );
 }
