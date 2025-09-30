a43e26d4c404dd32eb75c967bf2699eeb93766af
Brian Matheo <bmalvarezp@ufpso.edu.co>
2025-09-29 23:16:15 -0500

    CRUD of the product, all functional

--- src/component/protected-route.jsx
+++ src/component/protected-route.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 import Spinner from "react-bootstrap/Spinner";
@@ -10,13 +10,17 @@ export function ProtectedRoute({ children }) {
   const navigate = useNavigate();
 
   const [isLoading, setIsLoading] = useState(true);
-  auth.onAuthStateChanged((user) => {
-    if (!user) {
-      navigate("/login");
-    } else {
-      setIsLoading(false);
-    }
-  });
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        navigate("/login", { replace: true });
+      } else {
+        setIsLoading(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [navigate]);
 
   if (isLoading) {
     return (
